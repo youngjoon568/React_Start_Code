44eff82371abfc34c0b4092fa23e8c050384628a
YoungJoon <93526791+youngjoon568@users.noreply.github.com>
2022-11-12 14:51:54 +0900

    수정

--- client/src/component/App.js
+++ client/src/component/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './view/pages/Home';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
--- client/src/component/pages/HomePage.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import HomeLayout from '../templates/HomeLayout';
-
-export default function HomePage() {
-  return <HomeLayout />;
-};
